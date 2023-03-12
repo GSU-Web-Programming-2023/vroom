fa1645b239d964a72e17174b46727f1760e6ad9e
judahpaul16 <judahpaul16@gmail.com>
2023-03-11 19:46:37 -0500

    Created a route for POST requests

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(
     __name__,
@@ -6,9 +8,30 @@ app = Flask(
     static_folder='static'
 )
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'ID: {self.id}, Name {self.name}'
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/api/endpoint/', methods = ['POST'])
+def api():
+    data = request.get_json()
+    name = data['name']
+    response_data = {
+        'message': f'Hello, {name}!'
+    }
+    return response_data
 
 if __name__ == '__main__':
     app.run()
--- static/javascript/misc.js
+++ static/javascript/misc.js
@@ -16,6 +16,21 @@ window.addEventListener('load', function() {
     });
   });
 
+  // Hide HUD Minimap when M is pressed
+  window.addEventListener('load', function() {
+    document.addEventListener('keypress', function(event) {
+        if (event.key === 'm') {
+          // toggle the display of the element with id "myElement"
+          let controls = document.getElementById("minimap");
+          if (controls.style.display === "none") {
+            controls.style.display = "block";
+          } else {
+            controls.style.display = "none";
+          }
+        }
+      });
+    });
+
 // Hide chat bubble when clicked
 window.addEventListener('load', function() {
   chatBubble.addEventListener('click', function(event) {
@@ -29,4 +44,20 @@ window.addEventListener('load', function() {
       opacity -= opacity * 0.1;
     }, 15);
   });
+});
+
+// POST request
+window.addEventListener('load', function() {
+  const url = 'http://localhost:5000/api/endpoint/';
+  const data = { name : 'Judah Paul' };
+
+  response = fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => console.log(response))
+  .catch(error => console.error(error));
 });
