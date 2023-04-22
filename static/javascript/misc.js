9263bbd38f7a40d1094a6c42f81712d1082c595c
judahpaul16 <judahpaul16@gmail.com>
2023-04-21 21:54:57 -0400

    Styled pause menu for mobile

--- static/javascript/misc.js
+++ static/javascript/misc.js
@@ -12,16 +12,16 @@ function fadeOut(element) {
 
 // Hide dialogue when clicked
 let dialogue = document.getElementById('dialogue');
-window.addEventListener('load', function() {
-  dialogue.addEventListener('click', function(event) {
+window.addEventListener('load', function () {
+  dialogue.addEventListener('click', function (event) {
     fadeOut(this);
   });
 });
 
 // Show/Hide HUD controls when C is pressed
-window.addEventListener('load', function() {
-  document.addEventListener('keypress', function(event) {
-      if (event.key === 'c') {
+window.addEventListener('load', function () {
+  document.addEventListener('keypress', function (event) {
+    if (event.key === 'c') {
       let controls = document.getElementById("hud-controls");
       if (controls.style.opacity === "0") {
         controls.style.opacity = "1";
@@ -33,20 +33,30 @@ window.addEventListener('load', function() {
 });
 
 // Show/Hide Pause Menu when P is pressed
-function togglePauseMenu(){
+function togglePauseMenu() {
   let menu = document.getElementById("pause-menu");
   let hud = document.querySelector(".hud");
   if (menu.style.display === "none") {
-    menu.style.display = "flex";
+    if (window.innerWidth < 990) {
+      menu.style.display = "inline";
+      let mobileControls = document.querySelector(".mobile-controls-container");
+      mobileControls.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+    }
     hud.style.opacity = "0";
   } else {
     menu.style.display = "none";
     hud.style.opacity = "1";
+    if (window.innerWidth < 990) {
+      let mobileControls = document.querySelector(".mobile-controls-container");
+      mobileControls.style.display = "flex";
+    }
   }
 }
 
-window.addEventListener('load', function() {
-  document.addEventListener('keypress', function(event) {
+window.addEventListener('load', function () {
+  document.addEventListener('keypress', function (event) {
     if (event.key === 'p') {
       togglePauseMenu();
     }
@@ -58,7 +68,7 @@ function debugMenuEventListener() {
   let debugMenu = document.querySelector(".dg.ac");
   if (debugMenu) {
     debugMenu.style.display = "none";
-    document.addEventListener('keypress', function(event) {
+    document.addEventListener('keypress', function (event) {
       if (event.key === '\`') {
         if (debugMenu.style.display === "none") {
           debugMenu.style.display = "block";
@@ -79,7 +89,7 @@ let timer_Id;
 
 // Begin a timer that will reload the page after 1 minute of inactivity
 function beginTimer() {
-  timer_Id = setTimeout(function() {
+  timer_Id = setTimeout(function () {
     location.reload();
   }, inactivityTime * 1000);
 }
@@ -91,7 +101,7 @@ function resetTimer() {
 }
 
 // Start a timer that will check for inactivity at a specified interval
-setInterval(function() {
+setInterval(function () {
   // Add event listener to reset the timer on user interaction (moving mouse, pressing on a key, holding a key, clicking)
   document.addEventListener('mousemove', resetTimer);
   document.addEventListener('click', resetTimer);
